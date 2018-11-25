9c82193bccb4e97633ce9b24fbee4582395e2412
Thomas Gabauer <tgabauer@gmail.com>
2018-11-25 13:31:11 -0700

    testing layout

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-dark bg-primary">
+       <a className="navbar-brand" href="#">Echo Music</a>
+    </nav>
+  )
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+
+const Main = (props)=> {
+  return (
+    <div>
+      <h1 className="mainFont">This is a test</h1>
+    </div>
+  )
+}
+
+export default Main
