e80b94259eb7107498157056b7d1cc3d35e1311d
Thomas Gabauer <tgabauer@gmail.com>
2018-11-28 20:21:10 -0700

    added React Audio Player

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import ReactAudioPlayer from 'react-audio-player';
 
 const Footer = (props) => {
   let playListUrls = props.playListUrls
   return (
     <nav className="navbar navbar-dark bg-dark">
-     <audio src={playListUrls[0]} controls></audio>
+
       <a className="navbar-brand" href="#">&copy; 2018  Echo Music Streams</a>
+      <ReactAudioPlayer
+      src={playListUrls[1]}
+      controls/>
    </nav>
   )
 }
